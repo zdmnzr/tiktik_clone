8460581aea68708279321878026e9896dbfbab7a
zdmnzr <73174556+zdmnzr@users.noreply.github.com>
2022-07-19 22:12:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors:true,
+  },
   reactStrictMode: true,
   // swcMinify: true,
   images:{
     domains:['brucecai55520.gitee.io',
+    'yt3.ggpht.com',
               'lh3.googleusercontent.com',
                'image.baidu.com']
   }
